6114fa73f17bee8574c4f7d652a291b9c47edef7
Jonathan Alves <jonny.alves99@gmail.com>
2020-08-09 17:14:57 -0300

    teste

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("hello world");
+const Pool = require('pg').Pool;
+
+const main = async () => {
+
+    const pool = new Pool({
+        host: 'localhost',
+        port: 5432,
+        database: 'cursonode',
+        user: 'postgres',
+        password: '2013jota'
+    });
+
+    let client = await pool.connect(); 
+
+    await client.query("insert into tbclientes (tipo,nome) VALUES ('F','teste')")
+
+    client.end();
+
+    pool.end();
+
+}
+
